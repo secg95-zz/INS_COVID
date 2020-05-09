386c1f4cdc4070ea324276f02a754b2489bc04b8
Santiago Cortes <secg95@gmail.com>
2020-05-08 23:32:14 -0500

    simulations

---
+++ generate_samples.r
@@ -0,0 +1,31 @@
+"
+run simulations using bootstrapped data.
+"
+source("model_fitting.R")
+load("Flu2009.rda")
+load("CasosAjustados_Diagnostico.Rda")
+library(nloptr)
+
+observed_I = Flu2009$incidence$I
+# initialize beta
+raw_N = cumsum(observed_I)
+lagged_N = lag(raw_N)
+lagged_N[1] = 1
+beta0 = rep(0.3, length(observed_I)) # rep(mean(observed_I / lagged_N), length(observed_I))
+beta_min = min(observed_I / lagged_N)
+beta_max = max(observed_I / lagged_N)
+model = fit(observed_I, beta0=beta0, beta_min=beta_min, beta_max=beta_max,
+                lambda0=1/6, lambda_min=1/8, lambda_max=1/4, alpha=6.4e+13)
+simulados = get_expected_I( model$beta, model$N0, model$lambda)
+bootstraped_betas = list()
+# fit Nicolas's model
+for( i in 1:10){
+    # plot reproduction number series
+    # R = model$beta / model$lambda
+    I_bootstrapped = bootstrap_sample(simulados, length(model$beta), 3)
+    bootstraped_model = fit(I_bootstrapped[1:(length(I_bootstrapped)-1)], beta0=beta0, beta_min=beta_min, beta_max=beta_max,
+                lambda0=1/6, lambda_min=1/8, lambda_max=1/4, alpha=6.4e+13)
+    list.append(bootstraped_betas, i=bootstraped_model$beta)
+    #print(bootstraped_betas)
+    #plot(R, xlab="t", ylab="R(t)", type="l")
+}
