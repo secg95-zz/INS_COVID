ddc9a2e5c577ecf3cc8b9e756c22747a8e2b7552
Santiago Cortes <secg95@gmail.com>
2020-05-09 15:36:55 -0500

    fixes

--- generate_samples.r
+++ generate_samples.r
@@ -11,6 +11,7 @@ library(reshape2)
 
 observed_I = Flu2009$incidence$I
 # initialize beta
+num_simulations= 3
 raw_N = cumsum(observed_I)
 lagged_N = lag(raw_N)
 lagged_N[1] = 1
@@ -21,22 +22,23 @@ model = fit(observed_I, beta0=beta0, beta_min=beta_min, beta_max=beta_max,
                 lambda0=1/6, lambda_min=1/8, lambda_max=1/4, alpha=6.4e+13)
 simulados = get_expected_I( model$beta, model$N0, model$lambda)
 bootstraped_betas = list()
+print(simulados)
 # fit Nicolas's model
-for( i in 1:5){
+for( i in 1:num_simulations){
     # plot reproduction number series
     # R = model$beta / model$lambda
-    print(i)
-    I_bootstrapped = bootstrap_sample(simulados, length(observed_I), 3)
-    bootstraped_model = fit(I_bootstrapped, beta0=beta0, beta_min=beta_min, beta_max=beta_max,
+    bootstrapped_residual = bootstrap_sample(simulados, observed_I, length(observed_I), 3)
+    bootstraped_model = fit(observed_I + bootstrapped_residual, beta0=beta0, beta_min=beta_min, beta_max=beta_max,
                 lambda0=1/6, lambda_min=1/8, lambda_max=1/4, alpha=6.4e+13)
     bootstraped_betas <- list.append(bootstraped_betas, i=bootstraped_model$beta)
 }    
 
-bootstraped_betas <- list.append(bootstraped_betas, "time"=seq.int(length(model$beta)))
+
 df <- data.frame(matrix(unlist(bootstraped_betas), nrow=length(bootstraped_betas), byrow=T))
 df <- t(df)
-#colnames(df)[length(colnames(df))] <- "time"
-#print(df)
-#df <- melt(df ,  id.vars = 'time', variable.name = 'series')
-#print(df)
-#ggplot(df, aes(time,value)) + geom_line(aes(colour = series))
+colnames(df) <- c( 1:num_simulations)
+rownames(df) <- NULL
+df <- melt(df ,  id.vars = "time", variable.name = 'series')
+colnames(df) <- c('time', 'series', 'value')
+print(df)
+ggplot(df, aes(time,value)) + geom_line(aes(colour = series))
