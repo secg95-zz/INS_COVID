a6f6ea34cb02a2f406daf03f1b96cc9a6fb9f5b7
Santiago Cortes <secg95@gmail.com>
2020-05-26 19:08:53 -0500

    pre-merge

--- Colombia.R
+++ Colombia.R
@@ -2,6 +2,8 @@
 Fit Nicolas's model to the Colombia data.
 "
 library(RJSONIO)
+library(foreach)
+library(doParallel)
 source("model_fitting.r")
 source("visualization.r")
 
@@ -14,7 +16,7 @@ params = list(
   "beta_max" = 0.7
 )
 bootstrap_params = list(
-  "number_samples" = 20000,
+  "number_samples" = 1000,
   "window_size" = 10,
   "confidence" = 0.95
 )
@@ -45,7 +47,8 @@ dev.off()
 expected_I = get_expected_I(model$beta, model$N0, model$lambda)
 b_samples = bootstrap_samples(expected_I, observed_I,
                               bootstrap_params$number_samples,
-                              bootstrap_params$window_size)
+                              bootstrap_params$window_size,
+                              use_wa = TRUE)
 write(toJSON(bootstrap_params), paste(out_dir, "bootstrap_params.json", sep="/"))
 png(paste(out_dir, "I(t).png", sep="/"))
 plot_I_intervals(expected_I, observed_I, b_samples, confidence=bootstrap_params$confidence)
