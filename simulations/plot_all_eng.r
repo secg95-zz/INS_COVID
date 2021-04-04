d9c53a22739252763e022cfe3d60b1ea038a31a6
JFCeron <juanfceron@gmail.com>
2021-04-04 10:05:16 -0700

    Fixed color-to-model mapping in plots to maintain consistency

--- simulations/plot_all_eng.r
+++ simulations/plot_all_eng.r
@@ -17,11 +17,11 @@ plot_all = function(out_dir, simulation, bayesian_fit, ss_fit, poisson_fit) {
     scale_colour_manual(values = c('black'='black', 'blue'='blue'), name = '', 
                         labels = c(expression(I[N]), expression("E[I"[N]~"]"))) +
     xlab('t') +
-    ylab('Incidencias') +
+    ylab('Incidence') +
     theme(legend.title = element_blank(),legend.text=element_text(size=15), 
           axis.title=element_text(size=15), axis.text.x=element_text(size=12),
           axis.text.y=element_text(size=12), legend.position="bottom")
-  png(paste(out_dir, "I.png", sep="/"), pointsize=15, width=420, height=350)
+  png(paste(out_dir, "I_eng.png", sep="/"), pointsize=15, width=420, height=350)
   print(p)
   dev.off()
   
@@ -38,14 +38,14 @@ plot_all = function(out_dir, simulation, bayesian_fit, ss_fit, poisson_fit) {
   y_upper_bound = if (max(simulation$R) < 6) 7 else 8.5
   p =
     ggplot(data) +
-    geom_line(data = data, aes(x = dates, y = real, color = "Teórico")) +
-    geom_line(data = data, aes(x = dates, y = bayesian, color = "Bayesiano")) +
-    geom_line(data = data, aes(x = dates, y = ss, color = "Estado-Espacio")) +
+    geom_line(data = data, aes(x = dates, y = real, color = "Theoretical Model")) +
+    geom_line(data = data, aes(x = dates, y = bayesian, color = "Bayesian")) +
+    geom_line(data = data, aes(x = dates, y = ss, color = "State-Space")) +
     geom_line(data = data, aes(x = dates, y = poisson, color = "Poisson")) +
     scale_color_manual(values = c(
-      'Teórico' = 'black',
-      'Bayesiano' = 'blue',
-      'Estado-Espacio' = 'darkgreen',
+      'Theoretical Model' = 'black',
+      'Bayesian' = 'blue',
+      'State-Space' = 'darkgreen',
       'Poisson' = 'deeppink1'
     )) +
     xlab('t') +
@@ -54,7 +54,7 @@ plot_all = function(out_dir, simulation, bayesian_fit, ss_fit, poisson_fit) {
     theme(legend.title = element_blank(),legend.text=element_text(size=15),
           axis.title=element_text(size=15), axis.text.x=element_text(size=12),
           axis.text.y=element_text(size=12), legend.position="bottom")
-  png(paste(out_dir, "R.png", sep="/"), pointsize=15, width=420, height=350)
+  png(paste(out_dir, "R_eng.png", sep="/"), pointsize=15, width=420, height=350)
   print(p)
   dev.off()
   
@@ -63,12 +63,12 @@ plot_all = function(out_dir, simulation, bayesian_fit, ss_fit, poisson_fit) {
   data$poisson_3 <- poisson_fit[[3]]$R[1:aux_length]
   p =
     ggplot(data) +
-    geom_line(data = data, aes(x = dates, y = real, color = "Teórico")) +
+    geom_line(data = data, aes(x = dates, y = real, color = "Theoretical Model")) +
     geom_line(data = data, aes(x = dates, y = poisson, color = "Poisson")) +
     geom_line(data = data, aes(x = dates, y = poisson_2, color = "Poisson 1")) +
     geom_line(data = data, aes(x = dates, y = poisson_3, color = "Poisson 2")) +
     scale_color_manual(values = c(
-      'Teórico' = 'black',
+      'Theoretical Model' = 'black',
       'Poisson 1' = 'darkgreen',
       'Poisson 2' = 'blue',
       'Poisson' = 'deeppink1'
@@ -79,7 +79,7 @@ plot_all = function(out_dir, simulation, bayesian_fit, ss_fit, poisson_fit) {
     theme(legend.title = element_blank(),legend.text=element_text(size=15),
           axis.title=element_text(size=15), axis.text.x=element_text(size=12),
           axis.text.y=element_text(size=12), legend.position="bottom")
-  png(paste(out_dir, "R_Poisson.png", sep="/"), pointsize=15, width=420, height=350)
+  png(paste(out_dir, "R_Poisson_eng.png", sep="/"), pointsize=15, width=420, height=350)
   print(p)
   dev.off()
 }
@@ -99,28 +99,28 @@ for (scenario_dir in scenario_dirs) {
   # build MAPE table
   scenario_mape = fromJSON(paste(scenario_dir, "R_mape.json", sep="/"), simplify = TRUE)
   scenario_mape = scenario_mape[c("bayesian", "ss", "Poisson 2")]
-  names(scenario_mape) = c("Bayesiano", "Estado-Espacio", "Poisson")
+  names(scenario_mape) = c("Bayesian", "State-Space", "Poisson")
   mape_subtable = data.frame(t(scenario_mape))
   mape_subtable[["scenario"]] = toString(scenario_count)
   scenario_count = scenario_count + 1
   mape_table = if (is.null(mape_table)) mape_subtable else rbind(mape_table, mape_subtable)
 }
 # store raw MAPE table
-write.csv(mape_table, "simulations/mape.csv", row.names=FALSE)
+write.csv(mape_table, "simulations/mape_eng.csv", row.names=FALSE)
 # plot MAPE table
 mape_melt = melt(mape_table, value.name="mape")
 colnames(mape_melt) = c("scenario", "model", "mape")
 mape_melt$model = as.character(mape_melt$model)
-mape_melt$model[mape_melt$model == "Estado.Espacio"] = "Estado-Espacio"
+mape_melt$model[mape_melt$model == "State.Space"] = "State-Space"
 mape_melt$mape[mape_melt$mape > 2] = NaN
 p = ggplot(mape_melt) +
     geom_col(data=mape_melt, position="dodge", aes(x=scenario, y=mape, fill=model)) +
-    scale_fill_manual(values = c('blue', 'darkgreen', 'deeppink1')) +
-    xlab('Escenario') +
+    scale_fill_manual(values = c('blue', 'deeppink1', 'darkgreen')) +
+    xlab('Scenario') +
     ylab('MAPE(R)') +
     theme(legend.title = element_blank(), legend.text=element_text(size=15),
           axis.title=element_text(size=15), axis.text.x=element_text(size=12),
           axis.text.y=element_text(size=12), legend.position="bottom")
-png("simulations/mape.png", pointsize=15, width=630, height=350)
+png("simulations/mape_eng.png", pointsize=15, width=630, height=350)
 print(p)
 dev.off()
